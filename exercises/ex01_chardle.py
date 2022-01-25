2ca580b43b4e96c5ad4bed52c81e4820451fe0e3
Nathan Kern <ntkern12@email.unc.edu>
2022-01-24 22:46:44 -0500

    Completed Exercise 1

--- exercises/ex01_chardle.py
+++ exercises/ex01_chardle.py
@@ -6,13 +6,13 @@ __author__ = "730382185"
 xword = input("Enter a 5-character word: ")
 
 if len(xword) < 5 or len(xword) > 5:
-    print("Word must contain 5 characters")
+    print("Error: Word must contain 5 characters")
     exit()
 
 xletter = input("Enter a single character: ")
 
 if len(xletter) < 1 or len(xletter) > 1:
-    print("Character must be a single character.")
+    print("Error: Character must be a single character.")
     exit()
 
 print("Searching for " + xletter + " in " + xword)
@@ -35,7 +35,9 @@ if xword[4] == xletter:
     print(xletter + " found at index 4")
     xword_count = xword_count + 1
 
-if xword_count > 0:
+if xword_count == 1:
     print(str(xword_count) + " instance of " + xletter + " found in " + xword)
+elif xword_count > 1:
+    print(str(xword_count) + " instances of " + xletter + " found in " + xword)
 else:
     print("No instances of " + xletter + " found in " + xword)
