4c91e9f5d3a8a267a192f248cff1543a3d1aac68
Nathan Kern <ntkern12@email.unc.edu>
2022-01-30 21:50:36 -0500

    Completed Exercise 2

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -2,30 +2,22 @@
 
 __author__ = "730382185"
 
-
 secret_word = "python"
 
 num_letters = len(secret_word)
 
 your_word = input(f"What is your {num_letters}-letter guess? ")
 
-count = 0
+while (len(your_word) < num_letters or len(your_word) > num_letters):
+    your_word = input(f"That was not {num_letters} letters! Try again: ")
+
+index = 0
 
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
 
-index = 0
-
 results = ""
-
-while (len(your_word) < num_letters or len(your_word) > num_letters) and count < 5:
-    your_word = input(f"That was not {num_letters} letters! Try again: ")
-    count = count + 1
-
-if count == 5:
-    print("Not quite. Play again soon!")
-    exit()
 
 while index < num_letters:
     if your_word[index] == secret_word[index]:
