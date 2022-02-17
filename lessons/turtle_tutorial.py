443448dd199872a4a565e1a71ca49fa4e82220e2
Nathan Kern <ntkern12@email.unc.edu>
2022-02-16 19:40:50 -0500

    Complete

--- lessons/turtle_tutorial.py
+++ lessons/turtle_tutorial.py
@@ -1,4 +1,5 @@
-from turtle import Turtle, colormode, done
+from turtle import Turtle, done 
+# ,colormode
 leo: Turtle = Turtle()
 
 # leo.forward(50)
@@ -76,4 +77,5 @@ while (i < 3):
     bob.forward(side_length)
     bob.left(120)
     i = i + 1
-    side_length = side_length * 0.97
+    # side_length = side_length * 0.97
+done()
