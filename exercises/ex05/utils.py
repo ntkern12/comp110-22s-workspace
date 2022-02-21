d654da2825e0af8b38b61513cd42ebdc15bcb4ea
Nathan Kern <ntkern12@email.unc.edu>
2022-02-21 14:38:49 -0500

    Completed Exercise 5

--- exercises/ex05/utils.py
+++ exercises/ex05/utils.py
@@ -4,6 +4,7 @@ __author__ = "730382185"
 
 
 def only_evens(a_list: list) -> list:
+    """Return all items in a list that are even numbers."""
     new_list = []
     i = 0
     while i < len(a_list):
@@ -15,16 +16,8 @@ def only_evens(a_list: list) -> list:
     return(new_list)
 
 
-list_a = [1, 2, 3]
-list_b = [1, 5, 3]
-list_c = [4, 4, 4]
-
-print(only_evens(list_a))
-print(only_evens(list_b))
-print(only_evens(list_c))
-
-
 def sub(a_list_2: list, start: int, end: int) -> list:
+    """Return all items in a list between two indices."""
     new_list_2 = []
     j = 0
     while j < len(a_list_2):
@@ -38,18 +31,8 @@ def sub(a_list_2: list, start: int, end: int) -> list:
     return(new_list_2)
 
 
-list_a_2 = [10, 20, 30, 40]
-list_b_2 = [1, 5, 3, 8, 9, 10, 54]
-list_c_2 = []
-
-print(sub(list_a_2, 1, 3))
-print(sub(list_b_2, 2, 5))
-print(sub(list_c_2, 1, 3))
-print(sub(list_b_2, 8, 10))
-print(sub(list_b_2, 0, 0))
-
-
 def concat(a_list_3: list, b_list: list) -> list:
+    """Combine a pair of lists sequentially."""
     new_list_3 = []
     k = 0
     while k < len(a_list_3):
@@ -60,9 +43,3 @@ def concat(a_list_3: list, b_list: list) -> list:
         new_list_3.append(b_list[m])
         m = m + 1
     return(new_list_3)
-
-
-list_a_3 = [1, 2, 3]
-list_b_3 = [4, 5, 6]
-
-print(concat(list_a_3, list_b_3))
--- exercises/ex05/utils_test.py
+++ exercises/ex05/utils_test.py
@@ -6,36 +6,65 @@ from exercises.ex05.utils import only_evens, sub, concat
 
 
 def test_only_evens_1():
+    """Test with evens."""
     assert only_evens([1, 2, 3]) == [2]
 
 
 def test_only_evens_2():
+    """Test without evens."""
     assert only_evens([1, 5, 3]) == []
 
 
 def test_only_evens_3():
+    """Test with only evens."""
     assert only_evens([4, 4, 4]) == [4, 4, 4]
 
 
+def test_only_evens_4():
+    """Test with empty list."""
+    assert only_evens([]) == []
+
+
 def test_sub_1():
+    """Test with short list."""
     assert sub([10, 20, 30, 40], 1, 3) == [20, 30]
 
 
 def test_sub_2():
+    """Test with long list."""
     assert sub([1, 5, 3, 8, 9, 10, 54], 2, 5) == [3, 8, 9]
 
 
 def test_sub_3():
+    """Test for inputting empty list."""
     assert sub([], 1, 3) == []
 
 
 def test_sub_4():
+    """Test for incorrect indices."""
     assert sub([1, 5, 3, 8, 9, 10, 54], 8, 10) == []
 
 
 def test_sub_5():
+    """Test for too small indices."""
     assert sub([1, 5, 3, 8, 9, 10, 54], 0, 0) == []
 
 
+def test_sub_6():
+    """Test for inputting empty list and incorrect indices."""
+    assert sub([], 10, 0) == []
+
+
 def test_concat_1():
+    """Test with only numbers."""
     assert concat([1, 2, 3], [4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+
+
+def test_concat_2():
+    """Test with numbers and letters."""
+    assert concat(["a", "b", "c"], [1, 2, 3]) == ["a", "b", "c", 1, 2, 3]
+
+
+def test_concat_3():
+    """Test with empty lists."""
+    assert concat([], []) == []
